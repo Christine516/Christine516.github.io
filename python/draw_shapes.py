575b9af19d2072c50bc9e88627be431954cb038d
Christine516 <you@example.com>
2017-07-19 12:58:11 -0700

    portfolio

---
+++ python/draw_shapes.py
@@ -0,0 +1,16 @@
+from turtle import *
+import math
+# Set Up your screen and starting position.
+setup(500,300)
+'''x_pos = -250
+y_pos = -150
+t.setposition(x_pos, y_pos)'''
+
+color("red")
+for x in range(4):
+    forward(100)
+    right(90)
+
+
+# Close window on click.
+exitonclick()
